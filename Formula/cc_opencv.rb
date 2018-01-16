b84ad9cf41012b7cc55e77c0ca5ed59c1401b34f
Robert Marsa <rmarsa@clearcorrect.com>
2018-01-16 13:27:34 -0600

    renames classes correctly

--- Formula/cc_opencv.rb
+++ Formula/cc_opencv.rb
@@ -1,4 +1,4 @@
-class Opencv < Formula
+class CcOpencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
   url "https://github.com/opencv/opencv/archive/3.4.0.tar.gz"
