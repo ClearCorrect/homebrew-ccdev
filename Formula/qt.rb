34001a7c2715d2ba19757830433e39ecad6b2a7d
Richard Osborne <rosborne@clearcorrect.com>
2019-03-05 15:50:18 -0600

    Removing reference to mountain_lion to fix error on MacOSX
    * Updating to behave like qt.rb where it does not depend on mountain_lion as seen here: https://github.com/Homebrew/homebrew-core/blob/master/Formula/qt.rb
    * Fixes this error that occurs when doing a brew install with this formula:
    Error: clearcorrect/ccdev/qt: unknown version :mountain_lion

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -22,11 +22,6 @@ class Qt < Formula
   option "with-examples", "Build examples"
   option "without-proprietary-codecs", "Don't build with proprietary codecs (e.g. mp3)"
 
-  # OS X 10.7 Lion is still supported in Qt 5.5, but is no longer a reference
-  # configuration and thus untested in practice. Builds on OS X 10.7 have been
-  # reported to fail: <https://github.com/Homebrew/homebrew/issues/45284>.
-  depends_on :macos => :mountain_lion
-
   depends_on "pkg-config" => :build
   depends_on :xcode => :build
   depends_on "mysql" => :optional
